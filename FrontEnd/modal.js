bf09b86457d88640b68427416afbdafaee801f66
Nomera67 <y.rechtenstein@gmail.com>
2023-10-01 16:18:35 +0200

    almost finish styling of modals. Missing arrow in category selector and all API call need to be done

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,5 +1,7 @@
-const closeModal = document.getElementById("closeModal");
+const closeModal = document.querySelectorAll(".closeModal");
 const modal = document.getElementById("modal");
+const addModal = document.getElementById("modal2");
+const backModal = document.querySelector(".back-button");
 
 // Vérification de la connexion de l'utilisateur. Si une valeur est retournée c'est qu'il est connecté
 const utilisateurConnecte = !!window.localStorage.getItem("bearerAuth");
@@ -7,6 +9,7 @@ const utilisateurConnecte = !!window.localStorage.getItem("bearerAuth");
 
 if (utilisateurConnecte) {
     const editButtonContainer = document.getElementById("edit-button-container");
+    const addButton = document.getElementById("addButton");
 
     // Création du bouton d'édition
     const editButton = document.createElement("button");
@@ -26,12 +29,42 @@ if (utilisateurConnecte) {
         modal.setAttribute("aria-modal", "true");
     });
 
-    closeModal.addEventListener("click", () => {
+    addButton.addEventListener("click", () => {
         modal.close();
         modal.style.display = "none";
         modal.getAttribute("aria-hidden");
-        modal.setAttribute("aria-modal", "false")
+        modal.setAttribute("aria-modal", "false");
+        addModal.showModal();
+        addModal.style.display = "flex";
+        addModal.removeAttribute("aria-hidden");
+        addModal.getAttribute("aria-visible");
+        addModal.setAttribute("aria-modal", "true");
+    })
+
+    closeModal.forEach((button) => {
+        button.addEventListener("click", () => {
+            modal.close();
+            modal.style.display = "none";
+            modal.getAttribute("aria-hidden");
+            modal.setAttribute("aria-modal", "false")
+            addModal.close();
+            addModal.style.display = "none";
+            addModal.getAttribute("aria-hidden");
+            addModal.setAttribute("aria-modal", "false");
+        });
     });
+
+    backModal.addEventListener("click", () => {
+        modal.showModal();
+        modal.style.display = "flex";
+        modal.removeAttribute("aria-hidden");
+        modal.getAttribute("aria-visible");
+        modal.setAttribute("aria-modal", "true");
+        addModal.close();
+        addModal.style.display = "none";
+        addModal.getAttribute("aria-hidden");
+        addModal.setAttribute("aria-modal", "false");
+    })
 }
 
 function displayImagesInModal(data) {
